9a6d8b0033bcccf74c34140635f26037c1cff905
Tan Yin Loo <tan.yinloo@olaparty.sg>
2026-04-03 23:46:26 +0800

    feat: extract getOrCreateSessionId to module with tests

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { getOrCreateSessionId } from './lib/sessionId'
 import './style.css'
+
+window.__umpireSessionId = getOrCreateSessionId()
 
 const app = createApp(App)
 
